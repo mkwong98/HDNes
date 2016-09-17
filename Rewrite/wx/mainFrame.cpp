86e6921b3339d9655f4f6917bcd4837e486f2bd0
mkwong98 <mkwong98@gmail.com>
2016-09-18 00:42:32 +0800

    Add gui frame

---
+++ Rewrite/wx/mainFrame.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Jun 17 2015)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#include "mainFrame.h"
+
+///////////////////////////////////////////////////////////////////////////
+
+mainFrame::mainFrame( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	
+	this->Centre( wxBOTH );
+}
+
+mainFrame::~mainFrame()
+{
+}
---
+++ Rewrite/wx/mainFrame.h
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Jun 17 2015)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef __MAINFRAME_H__
+#define __MAINFRAME_H__
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/string.h>
+#include <wx/frame.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class mainFrame
+///////////////////////////////////////////////////////////////////////////////
+class mainFrame : public wxFrame 
+{
+	private:
+	
+	protected:
+	
+	public:
+		
+		mainFrame( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,300 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
+		
+		~mainFrame();
+	
+};
+
+#endif //__MAINFRAME_H__
