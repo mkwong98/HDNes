912783fe551a9a1f6b7d45e0332fec22b34b0132
mkwong98 <mkwong98@gmail.com>
2016-10-29 12:28:33 +0800

    Added memory read write module

--- Rewrite/core/memBus.cpp
+++ Rewrite/core/memBus.cpp
@@ -9,3 +9,12 @@ memBus::~memBus()
 {
     //dtor
 }
+
+Uint8 memRead(Uint16 address){
+	if(address < 0x2000){
+		return internalRAM[address & 0x07FF];
+	}
+
+}
+
+
--- Rewrite/core/memBus.h
+++ Rewrite/core/memBus.h
@@ -7,8 +7,12 @@
 class memBus : public corePart
 {
     public:
+        Uint8 internalRAM[0x800];
+
         memBus();
         ~memBus();
+
+        Uint8 memRead(Uint16 address);
 
     protected:
 
